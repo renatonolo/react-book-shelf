929b9226aa6ea7efa2f1cea35656690a07afa52f
Renato N. Lourenço <renatonolo@hotmail.com>
2018-09-26 19:13:50 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const { resolve, join } = require('path');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'public'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: join(__dirname, 'public'),
+        compress: false
+    },
+    stats: {
+        colors: true,
+        reasons: true
+    }
+}
